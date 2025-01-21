08e82a7055f7f67fba7b5b7ae41e48ccba34e00d
Joseph Cilenti <jecilenti@gmail.com>
2025-01-21 21:45:32 +0100

    Schwab Py barebones

---
+++ main.py
@@ -0,0 +1,32 @@
+from schwab import auth, client
+import json
+
+# this code should be ready
+# awaiting my app to be approved at schwab
+# still need to get a token when the app is ready for use
+
+with open('../../schwab_api_key.txt', 'r') as api_file:
+    schwab_api_key = api_file.readline()
+    print(schwab_api_key)
+
+with open('../../schwab_app_secret.txt', 'r') as secret_file:
+    schwab_app_secret = secret_file.readline()
+    print(schwab_app_secret)
+
+#schwab_app_token=
+
+
+api_key = schwab_api_key
+app_secret = schwab_app_secret
+
+callback_url = 'https://127.0.0.1:8182/'
+
+'''
+token_path = '/path/to/token.json'
+
+c = auth.easy_client(api_key, app_secret, callback_url, token_path)
+
+r = c.get_price_history_every_day('AAPL')
+r.raise_for_status()
+print(json.dumps(r.json(), indent=4))
+'''
